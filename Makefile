025d1afe0b746f31026701ea3e2b18919c1c7525
sortraev <anersholst@gmail.com>
2023-01-29 00:35:31 +0100

    start new fzf_fif

--- Makefile
+++ Makefile
@@ -1,20 +1,19 @@
 fzf_fif: fzf_fif.c
 	gcc -O3 $< -o $@
+run: fzf_fif
+	./$<
+
+
 fzf_changedir: fzf_wrap.c
 	gcc -O3 -DCHANGEDIR_NO_IGNORE=0 $< -o $@
 fzf_changedir_no_ignore: fzf_wrap.c
 	gcc -O3 -DCHANGEDIR_NO_IGNORE=1 $< -o $@
-
-
-run: fzf_fif
-	./$<
 
 debug: fzf_wrap.c
 	gcc -g3 -DCHANGEDIR_NO_IGNORE=0 $< -o $@
 
 check: debug
 	valgrind --track-fds=yes --leak-check=full ./$<
-
 
 
 clean:
--- fzf_fif_OLD.c
+++ fzf_fif_OLD.c
@@ -1,14 +1,9 @@
-
 #include <stdlib.h> // malloc, exit
 #include <stdio.h>  // printf, fprintf, fgets, fclose, etc.
 #include <string.h> // strerror, strcpy, strlen.
 #include <errno.h>  // errno.
 #include <unistd.h> // fork, pipe,
 #include <sys/wait.h>
-
-#ifndef CHANGEDIR_NO_IGNORE
-#define CHANGEDIR_NO_IGNORE 0
-#endif
 
 /*
  * unfinished ideas:
@@ -25,44 +20,96 @@
  *   child1: close reading; write to main; exec rg.
  */
 
+#ifndef CHANGEDIR_NO_IGNORE
+#define CHANGEDIR_NO_IGNORE 0
+#endif
+
 int __close(int fildes);
 int rg_child(int fds[2]);
-int fzf_child(int fds[2]);
+int cut_child(int fds[2]);
 int parent_f(int rg_r_end, int cut_w_end);
 
+int main(int argc, char **argv) {
+  (void) argc;
+  (void) argv;
 
-int fzf_child(int fds[2]) {
-  __close(fds[0]);
-  int r_end = fds[0];
-  int w_end = fds[1];
-
-  if (dup2(w_end, STDOUT_FILENO) == -1) {
-    fprintf(stderr, "dup2() error during stdout rerouting: %s\n",
-                     strerror(errno));
-    __close(r_end);
-    __close(w_end);
+  int fds[2];
+  if (pipe(fds) != 0) {
+    fprintf(stderr, "pipe() error: %s\n", strerror(errno));
     return 1;
   }
 
-  char *_argv[] = {
-    "fzf",
-    "--ansi",
-    "--reverse",
-    "--preview-window=~2,+{2}",
-    "--preview",
-    "bat --theme gruvbox-dark --color=always {1} --highlight-line {2}",
-    NULL
-  };
-
-  if (execvp(_argv[0], _argv) == -1) {
-    // TODO: how to handle exec() errors??
-    fprintf(stderr, "execvp() error: %s\n", strerror(errno));
+  pid_t childpid = fork();
+  if (childpid == -1) {
+    fprintf(stderr, "fork() error: %s\n", strerror(errno));
+    __close(fds[0]);
+    __close(fds[1]);
+    return 1;
   }
-  // this is only reached if exec does not succeed.
-  __close(r_end);
-  __close(w_end);
-  return 1;
+
+  else if (childpid == 0) {
+    /*
+     * CHILD.
+     */
+    (void) rg_child(fds);
+    return 0;
+  }
+
+  __close(fds[1]);
+  int rg_r_end = fds[0];
+
+  int fds2[2];
+  if (pipe(fds2) != 0) {
+    fprintf(stderr, "pipe() error: %s\n", strerror(errno));
+    return 1;
+  }
+
+  pid_t childpid2 = fork();
+  if (childpid2 == -1) {
+    fprintf(stderr, "fork() error: %s\n", strerror(errno));
+    // TODO: close fds
+    __close(rg_r_end);
+    __close(fds2[0]);
+    __close(fds2[1]);
+    return 1;
+  }
+  else if (childpid2 == 0) {
+    (void) cut_child(fds2);
+    return 0;
+  }
+
+  // TODO: correct??
+  __close(fds2[0]);
+  int cut_w_end = fds2[1];
+
+  /*
+   * PARENT.
+   */
+  (void) parent_f(rg_r_end, cut_w_end);
+
+  /*
+   * CHILD EXIT HANDLING.
+   * TODO: if execvp succeeded, then this code will never be reached. in this
+   * case, how is the child handled? I'm guessing waiting is only necessary when
+   * we are interested in/dependent on the child's result somehow.
+   *
+   * for now, let's just leave it here.
+   */
+  int stat_loc;
+  pid_t _childpid = waitpid(childpid, &stat_loc, 0);
+  if (_childpid == -1)
+    fprintf(stderr, "waitpid() error: %s\n", strerror(errno));
+  else if (_childpid != childpid) {
+    ;
+    // TODO: how to handle this?? probably not even necessary.
+  }
+  else if (!WIFEXITED(stat_loc))
+    fprintf(stderr, "Child exited abnormally with exit code %d\n",
+                    WEXITSTATUS(stat_loc));
+
+  return 0;
 }
+
 
 int rg_child(int fds[2]) {
   __close(fds[0]);
@@ -132,7 +179,7 @@ int fzf_child(int fds[2]) {
   return 1;
 }
 
-int nvim_child(int fds[2]) {
+int cut_child(int fds[2]) {
   __close(fds[1]);
   int r_end = fds[0];
 
@@ -189,7 +236,7 @@ int nvim_child(int fds[2]) {
 
 
 
-int parent_f(int fds[2]) {//int rg_r_end, int cut_w_end) {
+int parent_f(int rg_r_end, int cut_w_end) {
 
   if (dup2(rg_r_end, STDIN_FILENO) == -1) {
     fprintf(stderr, "dup2() error during stdin rerouting: %s\n",
@@ -209,14 +256,13 @@ int parent_f(int fds[2]) {//int rg_r_end, int cut_w_end) {
 
   // CHANGEDIR_NO_IGNORE
   char *_argv[] = {
-    "fzf",
+    "/usr/bin/fzf",
     "--ansi",
     "--reverse",
     "--preview-window=~2,+{2}",
     "--preview",
     "bat --theme gruvbox-dark --color=always {1} --highlight-line {2}",
-    NULL
-  };
+    NULL};
 
   if (execvp(_argv[0], _argv) == -1) {
     // TODO: how to handle exec() errors??
@@ -228,91 +274,10 @@ int parent_f(int fds[2]) {//int rg_r_end, int cut_w_end) {
   return 1;
 }
 
+
 int __close(int fildes) {
   int r = close(fildes);
   if (r != 0)
     fprintf(stderr, "close() error: %s\n", strerror(errno));
   return r;
-}
-
-
-int main(int argc, char **argv) {
-  (void) argc;
-  (void) argv;
-
-  int fds[2];
-  if (pipe(fds) != 0) {
-    fprintf(stderr, "pipe() error: %s\n", strerror(errno));
-    return 1;
-  }
-
-  pid_t childpid = fork();
-  if (childpid == -1) {
-    fprintf(stderr, "fork() error: %s\n", strerror(errno));
-    __close(fds[0]);
-    __close(fds[1]);
-    return 1;
-  }
-
-  else if (childpid == 0) {
-    /*
-     * CHILD.
-     */
-    (void) rg_child(fds);
-    return 0;
-  }
-
-  int rg_r_end = fds[0];
-  __close(fds[1]);
-
-  int fds2[2];
-  if (pipe(fds2) != 0) {
-    fprintf(stderr, "pipe() error: %s\n", strerror(errno));
-    return 1;
-  }
-
-  pid_t childpid2 = fork();
-  if (childpid2 == -1) {
-    fprintf(stderr, "fork() error: %s\n", strerror(errno));
-    // TODO: close fds
-    __close(rg_r_end);
-    __close(fds2[0]);
-    __close(fds2[1]);
-    return 1;
-  }
-  else if (childpid2 == 0) {
-    (void) cut_child(fds2);
-    return 0;
-  }
-
-  // TODO: correct??
-  __close(fds2[0]);
-  int cut_w_end = fds2[1];
-
-  /*
-   * PARENT.
-   */
-  (void) parent_f(rg_r_end, cut_w_end);
-
-  /*
-   * CHILD EXIT HANDLING.
-   * TODO: if execvp succeeded, then this code will never be reached. in this
-   * case, how is the child handled? I'm guessing waiting is only necessary when
-   * we are interested in/dependent on the child's result somehow.
-   *
-   * for now, let's just leave it here.
-   */
-  int stat_loc;
-  pid_t _childpid = waitpid(childpid, &stat_loc, 0);
-  if (_childpid == -1)
-    fprintf(stderr, "waitpid() error: %s\n", strerror(errno));
-  else if (_childpid != childpid) {
-    ;
-    // TODO: how to handle this?? probably not even necessary.
-  }
-  else if (!WIFEXITED(stat_loc))
-    fprintf(stderr, "Child exited abnormally with exit code %d\n",
-                    WEXITSTATUS(stat_loc));
-
-  return 0;
 }
